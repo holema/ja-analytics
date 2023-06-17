6c37e8b9c480d3484a4a3747684c04883a32a8c6
Entwicklung <entwicklung@h2-invent.com>
2023-06-17 11:27:36 +0200

    add admin dashboard

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AnalyticsDataRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    public function __construct(
+        private EntityManagerInterface  $entityManager,
+        private AnalyticsDataRepository $analyticsDataRepository,
+    )
+    {
+    }
+
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        $data = $this->analyticsDataRepository->findAll();
+        return $this->render('admin/index.html.twig', [
+            'data'=>$data
+        ]);
+    }
+}
